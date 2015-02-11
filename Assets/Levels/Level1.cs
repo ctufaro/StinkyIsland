821f6fdd3619d9738d06d4820064d86143930de4
JTangowski <Jtangowski@gmail.com>
2015-02-10 20:18:36 -0500

    Multiple Monsters

---
+++ Assets/Levels/Level1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1 : MonoBehaviour {
+    int MonsterCounter = 0;
+	// Use this for initialization
+	void Start () {
+        AddMonster(new Vector2(5.5f,13.5f));
+        AddMonster(new Vector2(-4.2f,13.22f));
+        AddMonster(new Vector2(-4.2f,5.47f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void AddMonster(Vector2 position)
+    {
+        ++MonsterCounter;
+        GameObject newMonster = Resources.Load("MonsterPrefab") as GameObject;
+        newMonster.name = "Monster"+MonsterCounter.ToString();
+        Rigidbody2D nm = Instantiate(newMonster.GetComponent<Rigidbody2D>(), position, Quaternion.identity) as Rigidbody2D;
+
+        nm.renderer.sortingLayerName = "Monster";
+    }
+}
